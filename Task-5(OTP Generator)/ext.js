0b85ecde09c5951db6c6482264eff01358c31462
Vijaya Kumar Annam <118079473+Vijaykumar069@users.noreply.github.com>
2023-09-17 03:56:41 +0530

    ext.js

--- Task-5(OTP Generator)/ext.js
+++ Task-5(OTP Generator)/ext.js
@@ -1,4 +1,4 @@
-    // Import the functions you need from the SDKs you need
+  /**<!--  // Import the functions you need from the SDKs you need
     import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
     import {
       getAuth,
@@ -23,5 +23,38 @@
     const app = initializeApp(firebaseConfig);
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider(app);
+    const phoneNumber = getPhoneNumberFromUserInput();
+    const appVerifier = window.recaptchaVerifier;-->**/
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import {
+      getAuth,
+      signInWithPopup,
+      GoogleAuthProvider,
+      signInWithPhoneNumber,
+      GithubAuthProvider,
+      FacebookAuthProvider,
+    } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCZ5yP3kKTJNcgdGaotGMrO2qgg7VWguIo",
+  authDomain: "otp-generator-aa0da.firebaseapp.com",
+  projectId: "otp-generator-aa0da",
+  storageBucket: "otp-generator-aa0da.appspot.com",
+  messagingSenderId: "1064692129424",
+  appId: "1:1064692129424:web:b4d8b6faa266dcdb6c0215",
+  measurementId: "G-YFEDY197LK"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const provider = new GoogleAuthProvider(app);
     const phoneNumber = getPhoneNumberFromUserInput();
     const appVerifier = window.recaptchaVerifier;
