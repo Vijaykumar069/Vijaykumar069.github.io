c57e51b293a06cc7d684879d731acc3bf00dd137
Vijaya Kumar Annam <118079473+Vijaykumar069@users.noreply.github.com>
2023-09-17 04:02:35 +0530

    firebase.js

--- Task-5(OTP Generator)/firebase.js
+++ Task-5(OTP Generator)/firebase.js
@@ -4,8 +4,8 @@
   // github - d4e579958006f02985aa247c654aadbbf3796eef
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -23,4 +23,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
