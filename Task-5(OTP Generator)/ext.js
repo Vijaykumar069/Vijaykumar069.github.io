a9506681abf44adf6e2d74c9268e8ef2359132d3
Vijaya Kumar Annam <118079473+Vijaykumar069@users.noreply.github.com>
2023-09-17 10:33:28 +0530

    ext.js

--- Task-5(OTP Generator)/ext.js
+++ Task-5(OTP Generator)/ext.js
@@ -27,8 +27,7 @@
     const appVerifier = window.recaptchaVerifier;-->**/
 
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import {
@@ -56,5 +55,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const provider = new GoogleAuthProvider(app);
-    const phoneNumber = getPhoneNumberFromUserInput();
-    const appVerifier = window.recaptchaVerifier;
+const phoneNumber = getPhoneNumberFromUserInput();
+const appVerifier = window.recaptchaVerifier;
